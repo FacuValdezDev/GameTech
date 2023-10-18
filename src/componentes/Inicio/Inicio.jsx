c1adffe431ebb4b6bfdaef0e51273116b29fbba9
FacundoValdezDev <facundo_valdez@outlook.com.ar>
2023-10-18 13:36:03 -0300

    home changes and services array

--- src/componentes/Inicio/Inicio.jsx
+++ src/componentes/Inicio/Inicio.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
+/* import OutletProduct from "../Outlet/Outlet"; */
 import "./Inicio.css";
-const Inicio = () => {
+import HomeProducts from "../Services/services";
+
+const Home = () => {
+  const [HomeProducts, setHomeProducts] = useState(HomeProducts);
   return (
     <div>
       <div className="mainBanner">
@@ -17,6 +21,63 @@ const Inicio = () => {
           <div className="imgContainer">
             <img src="src\assets\img\banner1.png" alt="computer" />
           </div>
+        </div>
+      </div>
+      <div className="typeCategory">
+        <div className="typeContainer">
+          <div className="typeBox">
+            <div className="typeImg">
+              <img src="src\assets\img\ryzen7-bg.png" alt="cpu" />
+            </div>
+            <div className="numType">
+              <p>Procesadores</p>
+            </div>
+          </div>
+          <div className="typeBox">
+            <div className="typeImg">
+              <img src="src\assets\img\ryzen7-bg.png" alt="gpu" />
+            </div>
+            <div className="numType">
+              <p>Placas de video</p>
+            </div>
+          </div>
+          <div className="typeBox">
+            <div className="typeImg">
+              <img src="src\assets\img\ryzen7-bg.png" alt="motherboards" />
+            </div>
+            <div className="numType">
+              <p>Motherboards</p>
+            </div>
+          </div>
+          <div className="typeBox">
+            <div className="typeImg">
+              <img src="src\assets\img\ryzen7-bg.png" alt="rams" />
+            </div>
+            <div className="numType">
+              <p>Memoria Ram</p>
+            </div>
+          </div>
+          <div className="typeBox">
+            <div className="typeImg">
+              <img src="src\assets\img\ryzen7-bg.png" alt="disk" />
+            </div>
+            <div className="numType">
+              <p>Almacenamiento</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="outlet">
+        <div className="outContainer">
+          {HomeProducts.map((curElm) => {
+            return (
+              <div className="outletBox" key={curElm.id}>
+                <div className="outletImg">
+                  <img src={curElm.Img} alt={curElm.Title} />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/componentes/Outlet/Outlet.jsx
+++ src/componentes/Outlet/Outlet.jsx
@@ -1,142 +1,108 @@
-const HomeProducts = [
+const OutletProduct = [
   {
     id: "1",
     name: "Ryzen 3 3200G 4.6GHz Turbo + Radeon Vega 8 AM4 Wraith Stealth Cooler",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "2",
     name: "Ryzen 3 4100 - No Cooler OEM",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "3",
     name: "Ryzen 5 3600 4.2GHz Turbo AM4 Wraith Stealth Cooler",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "4",
     name: "Ryzen 5 5600x 4.6GHZ Turbo + Wraith Stealth Cooler",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "5",
     name: "Ryzen 7 5700G 4.6GHz Turbo + Wraith Stealth Cooler",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "6",
     name: "Ryzen 7 5700X 4.6GHz Turbo AM4 - No Cooler",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "7",
     name: "Ryzen 7 7700 5.3GHz Turbo AM5",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "8",
     name: "Ryzen 7 5700G",
     price: "1400",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "2",
     name: "I7 10700",
     price: "1500",
     category: "Cpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "3",
     name: "RTX 3080",
     price: "2000",
     category: "Gpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "4",
     name: "RX 6700 XT",
     price: "1800",
     category: "Gpu",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "5",
     name: "X570 ASUS STRIX",
     price: "1000",
     category: "Motherboard",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "6",
     name: "A520 ASUS STRIX",
     price: "900",
     category: "Motherboard",
-    imgurl: "../src/assets/img/ryzen7.png",
-    quantity: 1,
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "7",
     name: "16GB DDR4 HYPERX",
     price: "500",
     category: "Memory-ram",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
   {
     id: "8",
     name: "Nvme2g 480GB",
     price: "300",
     category: "Disk",
-    imgurl: "../src/assets/img/ryzen7.png",
+    img: "../src/assets/img/ryzen7.png",
   },
 ];
-
-//getProduct
-
-/* export const getProduct = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      //buscar el producto
-      const product = products.find((p) => p.id === id);
-
-      //si existe el producto
-      if (product) {
-        resolve(product);
-      } else {
-        reject("No existe el producto");
-      }
-    }, 1000);
-  });
-};
-
-export const getProducts = (category) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const productFiltered = category
-        ? products.filter(
-            (product) => product.category.toLowerCase() === category,
-          )
-        : products;
-
-      resolve(productFiltered);
-    }, 1000);
-  });
-};
- */
-export default HomeProducts;
+export default OutletProduct;
