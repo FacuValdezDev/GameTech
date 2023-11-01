726667dea7ed822f86c8526180c5ba19f53803eb
FacundoValdezDev <facundo_valdez@outlook.com.ar>
2023-11-01 16:57:27 -0300

    cart styles
    agregue estilos al carrito de compras y funciones

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -3,6 +3,52 @@ import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import "./Cart.css";
 const Cart = ({ cart, setCart }) => {
+  //aumentar qty
+  const incqty = (product) => {
+    const exsit = cart.find((x) => {
+      return x.id === product.id;
+    });
+    setCart(
+      cart.map((curElm) => {
+        return curElm.id === product.id
+          ? { ...exsit, qty: exsit.qty + 1 }
+          : curElm;
+      }),
+    );
+  };
+
+  //bajar qty
+  const decqty = (product) => {
+    const exsit = cart.find((x) => {
+      return x.id === product.id;
+    });
+    setCart(
+      cart.map((curElm) => {
+        return curElm.id === product.id
+          ? { ...exsit, qty: exsit.qty - 1 }
+          : curElm;
+      }),
+    );
+  };
+
+  //sacar producto
+  const removeproduct = (product) => {
+    const exsit = cart.find((x) => {
+      return x.id === product.id;
+    });
+    if (exsit.qty > 0) {
+      setCart(
+        cart.filter((x) => {
+          return x.id !== product.id;
+        }),
+      );
+    }
+  };
+  //total price
+  const Totalprice = cart.reduce(
+    (price, item) => price + item.qty * item.price,
+    0,
+  );
   return (
     <div>
       <div className="cartContainer">
@@ -10,9 +56,8 @@ const Cart = ({ cart, setCart }) => {
           <div className="emptyCart">
             <h2 className="empty">El carrito esta vacio</h2>
             <Link to="/category/products" className="emptyCartbtn">
-              Comprar Más
+              Comprar ahora
             </Link>
-            <button>Vaciar Carrito</button>
           </div>
         )}
         <div className="cartContant">
@@ -23,21 +68,39 @@ const Cart = ({ cart, setCart }) => {
                   <img src={curElm.img} alt={curElm.name} />
                 </div>
                 <div className="cartDetail">
-                  <h4>{curElm.category}</h4>
-                  <h3>{curElm.name}</h3>
-                  <p>${curElm.price}</p>
-                  <div className="qty">
-                    <button className="incqty">+</button>
-                    <h4>sub total_: ${curElm.price * curElm.qty}</h4>
+                  <div className="cartInfo">
+                    <h4>{curElm.category}</h4>
+                    <h3>{curElm.name}</h3>
+                    <p>Precio: ${curElm.price}</p>
+                    <div className="qty">
+                      <button className="incqty" onClick={() => incqty(curElm)}>
+                        +
+                      </button>
+                      <input type="text" value={curElm.qty} />
+                      <button className="decqty" onClick={() => decqty(curElm)}>
+                        -
+                      </button>
+                    </div>
+                    <h4 className="subTotal">
+                      Sub-total: ${curElm.price * curElm.qty}
+                    </h4>
                   </div>
-                  <button>
-                    <AiOutlineClose />
-                  </button>
+                  <div className="close">
+                    <button>
+                      <AiOutlineClose onClick={() => removeproduct(curElm)} />
+                    </button>
+                  </div>
                 </div>
               </div>
             );
           })}
         </div>
+        {cart.length > 0 && (
+          <div>
+            <h2 className="totalprice">Precio Total: ${Totalprice}</h2>
+            <button>Finalizar pedido</button>
+          </div>
+        )}
       </div>
     </div>
   );
